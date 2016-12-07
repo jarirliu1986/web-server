d23c2516ac81a0ffda8dbb3f5094d06d8d218726
jarirliu1986 <289374247@qq.com>
2016-12-06 17:29:33 -0800

    2nd

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+app.get('/about', function (req, res) {
+	res.send('About us')
+})
+app.use(express.static(__dirname + '/public'));
+app.listen(PORT, function () {
+	console.log('express server is starting at ' + PORT);
+});
